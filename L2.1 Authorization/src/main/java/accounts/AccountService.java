33c36d640943050b8532d727d4d4a9e43dde438b
Bohdan Lozitskey <bondxxl01@gmail.com>
2018-06-28 22:32:23 +0300

    Server lesson 2

--- L2.1 Authorization/src/main/java/accounts/AccountService.java
+++ L2.1 Authorization/src/main/java/accounts/AccountService.java
@@ -3,20 +3,11 @@ package accounts;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * @author v.chibrikov
- *         <p>
- *         Пример кода для курса на https://stepic.org/
- *         <p>
- *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
- */
 public class AccountService {
-    private final Map<String, UserProfile> loginToProfile;
-    private final Map<String, UserProfile> sessionIdToProfile;
+    private final Map <String, UserProfile> loginToProfile;
 
     public AccountService() {
-        loginToProfile = new HashMap<>();
-        sessionIdToProfile = new HashMap<>();
+        loginToProfile = new HashMap <>();
     }
 
     public void addNewUser(UserProfile userProfile) {
@@ -24,18 +15,8 @@ public class AccountService {
     }
 
     public UserProfile getUserByLogin(String login) {
-        return loginToProfile.get(login);
+            return loginToProfile.get(login);
+
     }
 
-    public UserProfile getUserBySessionId(String sessionId) {
-        return sessionIdToProfile.get(sessionId);
-    }
-
-    public void addSession(String sessionId, UserProfile userProfile) {
-        sessionIdToProfile.put(sessionId, userProfile);
-    }
-
-    public void deleteSession(String sessionId) {
-        sessionIdToProfile.remove(sessionId);
-    }
 }
--- L2.1 Authorization/src/main/java/main/Main.java
+++ L2.1 Authorization/src/main/java/main/Main.java
@@ -1,33 +1,22 @@
 package main;
 
 import accounts.AccountService;
-import accounts.UserProfile;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.SessionsServlet;
-import servlets.UsersServlet;
+import servlets.SignInServlet;
+import servlets.SignUpServlet;
 
-/**
- * @author v.chibrikov
- *         <p>
- *         Пример кода для курса на https://stepic.org/
- *         <p>
- *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
- */
 public class Main {
     public static void main(String[] args) throws Exception {
         AccountService accountService = new AccountService();
 
-        accountService.addNewUser(new UserProfile("admin"));
-        accountService.addNewUser(new UserProfile("test"));
-
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/api/v1/users");
-        context.addServlet(new ServletHolder(new SessionsServlet(accountService)), "/api/v1/sessions");
+        context.addServlet(new ServletHolder(new SignUpServlet(accountService)), "/signup");
+        context.addServlet(new ServletHolder(new SignInServlet(accountService)), "/signin");
 
         ResourceHandler resource_handler = new ResourceHandler();
         resource_handler.setResourceBase("public_html");
@@ -39,6 +28,7 @@ public class Main {
         server.setHandler(handlers);
 
         server.start();
+        System.out.println("Server started");
         server.join();
     }
 }
